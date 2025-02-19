cf3d5eb1c1b253aeadfcff41905269652f22043b
Magdiel Mora <m41k80@icloud.com>
2025-02-18 18:54:04 -0600

    m

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -1,9 +1,16 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect } = require('@playwright/test');
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5000')
-    await expect(page.getByText('ivysaur')).toBeVisible()
-    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
-  })
-})
+    await page.goto('http://localhost:5000');
+    await page.waitForSelector('text=ivysaur'); 
+    await expect(page.getByText('ivysaur')).toBeVisible();
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible();
+  });
+
+  test('can navigate to a specific Pokémon page', async ({ page }) => {
+    await page.goto('http://localhost:5000');
+    await page.click('text=ivysaur'); 
+    await expect(page.getByText('chlorophyll')).toBeVisible();
+  });
+});
