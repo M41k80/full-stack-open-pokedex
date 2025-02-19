0ba16b0cbdca39c625bee7d347bad7c094dbfdea
Magdiel Mora <m41k80@icloud.com>
2025-02-19 16:39:25 -0600

    fixed implementation broke

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
-// Obtén el puerto de la variable de entorno o usa el puerto 5000 por defecto
+
 const PORT = process.env.PORT || 5000;
 
-// Sirve los archivos estáticos de la carpeta "dist"
-app.use(express.static(path.join(__dirname, 'dist')));
 
-// Maneja todas las rutas y redirige al archivo "index.html"
+app.use(express.static(path.join(__dirname, 'dist')))
+
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw new Error('error...')
+  res.send('OK')
+})
+
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 // Inicia el servidor
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Servidor iniciado en el puerto ${PORT}`);
-});
+  
+  console.log(`Servidor iniciado en el puerto ${PORT}`)
+})
+
+
